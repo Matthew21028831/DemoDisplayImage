d4388a0a3f9cd058136e090f0c05b5eb8a11916c
Matthew21028831 <21028831@myrp.edu.sg>
2022-07-29 11:11:58 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c36/id21028831/demodisplayimage/MainActivity.java
@@ -0,0 +1,24 @@
+package sg.edu.rp.c36.id21028831.demodisplayimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv = findViewById(R.id.iv);
+
+        String imageUrl = "https://i.imgur.com/tGbaZCY.jpg";
+        Picasso.with(this).load(imageUrl).into(iv);
+    }
+}
